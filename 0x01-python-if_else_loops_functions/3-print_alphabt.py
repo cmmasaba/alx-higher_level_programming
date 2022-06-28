099901b7ee7c6007e51727e5a5193dc6f1a75413
Collins Mmasaba <mmasaba3142@gmail.com>
2022-06-28 22:27:49 +0000

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,4 +4,4 @@ for letter in range (97, 123):
     if (letter == 101 or letter == 113):
         continue
     else:
-        print(f"{letter:c}", end = "")
+        print("{:c}".format(letter), end="")
